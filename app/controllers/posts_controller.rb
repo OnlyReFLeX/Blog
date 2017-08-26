74b11e45619fe2a20c0d40c76d9120caf22fee05
OnlyReFLeX <reflexonly@gmail.com>
2017-08-26 21:48:59 +0300

    edit posts controller & new.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
