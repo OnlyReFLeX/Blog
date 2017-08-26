3639699acd11bd43375cdbcfb9368f9c9865b8d7
OnlyReFLeX <reflexonly@gmail.com>
2017-08-26 22:20:23 +0300

    add edit, show, delete methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,20 +8,39 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id]).destroy
+    redirect_to posts_path
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
