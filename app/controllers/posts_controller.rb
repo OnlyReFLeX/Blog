86e17752e390d8412b11237f933892d84ea164d8
OnlyReFLeX <reflexonly@gmail.com>
2017-09-04 00:45:42 +0300

    edit and delete only creator

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,11 +21,20 @@ class PostsController < ApplicationController
   # Редактирование поста
   def edit
     get_params_id
+    if !user_signed_in? #ЕСЛИ ВОЙДЕН ТО FALSE ЕСЛИ НЕТ ТО TRUE
+      redirect_to posts_path
+    elsif @post.user_id == current_user.id #СРАВНЕНИЕ ЕГО ЛИ ПОСТ
+    else
+      redirect_to posts_path
+    end
   end
 
   def update
     get_params_id
-    if @post.update_attributes(post_params)
+    if !user_signed_in? #ЕСЛИ ВОЙДЕН ТО FALSE ЕСЛИ НЕТ ТО TRUE
+      redirect_to posts_path
+    elsif @post.user_id == current_user.id #СРАВНЕНИЕ ЕГО ЛИ ПОСТ
+      @post.update_attributes(post_params)
       redirect_to @post
     else
       render :edit
@@ -34,8 +43,15 @@ class PostsController < ApplicationController
 
   # Удаление поста
   def destroy
-    get_params_id.destroy
-    redirect_to posts_path
+    get_params_id
+    if !user_signed_in? #ЕСЛИ ВОЙДЕН ТО FALSE ЕСЛИ НЕТ ТО TRUE
+      redirect_to posts_path
+    elsif @post.user_id == current_user.id #СРАВНЕНИЕ ЕГО ЛИ ПОСТ
+      @post.destroy
+      redirect_to posts_path
+    else
+      redirect_to posts_path
+    end
   end
 
   # Просмотр поста
