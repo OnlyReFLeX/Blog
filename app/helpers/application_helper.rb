84dfe05e0d071ea7d19913bc2e273a11ec3c5661
OnlyReFLeX <reflexonly@gmail.com>
2017-11-20 22:47:15 +0300

    last online 15 min

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def last_seen_at_or_online(last_seen_at)
-    if last_seen_at < 3.minutes.ago
+    if last_seen_at < 15.minutes.ago
       "Был в сети: #{last_seen_at.try(:strftime, "%d.%m.%Y %H:%M")}"
     else
       "Online"
