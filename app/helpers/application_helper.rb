d7b07f39c256605a537789ea648441d96975099c
OnlyReFLeX <reflexonly@gmail.com>
2017-11-20 22:51:07 +0300

    UTC +3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   def last_seen_at_or_online(last_seen_at)
+    last_seen_at = last_seen_at.in_time_zone('Moscow')
     if last_seen_at < 15.minutes.ago
       "Был в сети: #{last_seen_at.try(:strftime, "%d.%m.%Y %H:%M")}"
     else
